8db637e66479d8486970666710fa8831a116c9eb
ahmed <ahmedgamalfathy39@gmail.com>
2025-09-30 12:38:47 +0300

    upload edit userResource

---
+++ app/Http/Resources/Devices/DeviceTime/AllDeviceTimeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Devices\DeviceTime;
+
+use App\Http\Resources\Product\DeviceTimeResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AllDeviceTimeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'deviceTimes' => DeviceTimeResource::collection($this->resource->items()),
+            // 'perPage'      => $this->resource->count(),
+            // 'nextPageUrl'  => $this->resource->nextPageUrl(),
+            // 'prevPageUrl'  => $this->resource->previousPageUrl(),
+        ];
+    }
+}
