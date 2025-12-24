3a5ba6308d52fd34a11ae1b3f96a497108449c35
Ahmed Gamal <ahmed@elmotech.it>
2025-12-24 11:30:06 +0200

    upload session group

--- app/Services/Select/SessionSelectService.php
+++ app/Services/Select/SessionSelectService.php
@@ -2,6 +2,7 @@
 namespace App\Services\Select;
 
 use App\Models\Daily\Daily;
+use App\Enums\SessionDevice\SessionDeviceEnum;
 use App\Models\Timer\SessionDevice\SessionDevice;
 
 class SessionSelectService
@@ -10,8 +11,8 @@ class SessionSelectService
     public function getSessionGroup()
     {
         return  SessionDevice::on('tenant')->whereHas('bookedDevices', function ($q) {
-        $q->where('status','!=',0); 
-        })->get(['id as value', 'name as label']);
+        $q->where('status','!=',0);
+        })->where('type',SessionDeviceEnum::GROUP->value)->get(['id as value', 'name as label']);
     }
 
 }
