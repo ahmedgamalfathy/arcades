66e542e8b5fdf31345ff9d91f5955f9b7432917e
Ahmed Gamal <ahmed@elmotech.it>
2026-01-20 15:59:14 +0200

    updated the price session in dailyOpen

--- app/Http/Resources/Timer/SessionDevice/Daily/AllSessionIncomeResource.php
+++ app/Http/Resources/Timer/SessionDevice/Daily/AllSessionIncomeResource.php
@@ -24,7 +24,7 @@ class AllSessionIncomeResource extends JsonResource
         return [
             'sessionId'=>$this->id,
             'sessioName'=> $sessionName,
-            'totalPriceSession'=>$this->bookedDevices->sum('period_cost'),
+            'totalPriceSession'=>$this->bookedDevices->sum('actual_paid_amount')??0,
             'createdAt' =>Carbon::parse($this->created_at)->format('d-m-Y'),
             'timeCreated' =>Carbon::parse($this->created_at)->format('H:i'),
             'periodCost'=>$this->bookedDevices->sum('actual_paid_amount')??"",
